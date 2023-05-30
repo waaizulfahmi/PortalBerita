c590c51e4c59dfa6ce8301f4b372e0c41d3b042d
waaizulfahmi <wandaaizulf@gmail.com>
2023-05-30 19:06:10 +0700

    added copy url button

--- resources/js/Pages/ReadNews/Read.jsx
+++ resources/js/Pages/ReadNews/Read.jsx
@@ -1,6 +1,7 @@
 import { Link } from "@inertiajs/react";
 import parser from "html-react-parser";
 import { useState, useEffect } from "react";
+import { render } from "react-dom";
 
 const Read = (props) => {
     // console.log(Object.keys(news));
@@ -8,8 +9,32 @@ const Read = (props) => {
 
     async function copyToClip() {
         await navigator.clipboard.writeText(location.href);
+        alertCopy();
         setCopySuccess("Copied");
     }
+
+    const alertCopy = () => {
+        render(
+            <div className="alert alert-info shadow-lg">
+                <div>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        className="stroke-current flex-shrink-0 w-6 h-6"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                        ></path>
+                    </svg>
+                    <span>{copySuccess}</span>
+                </div>
+            </div>
+        );
+    };
 
     console.log(props.news);
     const dateString = props.news.created_at;
